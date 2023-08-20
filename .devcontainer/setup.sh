0634842bb677f82bfd8c007f6c0c5300b4e0758c
Mike Leach (Wander) <mike@bamboofin.tech>
2023-08-20 14:03:49 -0500

    rocketpool setup wip

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -4,13 +4,15 @@
 
 cd ~
 
+echo Installing RP service...
 rocketpool --debug service install -y -p .rocketpool
+
 cp bin/hyperdrive/.devcontainer/user-settings.yml .rocketpool/user-settings.yml
 
-#sudo dockerd && containerd
-
+echo Starting RP service...
 rocketpool --debug service start -y --ignore-slash-timer
 
-rocketpool --debug wallet init -p thisisabigtest --confirm-mnemonic
+echo Initializing RP node wallet...
+rocketpool -s --debug wallet init -p thisisabigtest --confirm-mnemonic
 
 echo "{::} Hyperdrive development environment enabled! {::}\n"
