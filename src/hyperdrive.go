dad5885b4d6921ea38e2b385b54a2159f1c545db
mryamz <teddy.clapp@gmail.com>
2023-10-24 00:24:15 -0700

    [Feature] Add Cobra and pattern for making commands (#8)

--- src/hyperdrive.go
+++ src/hyperdrive.go
@@ -2,15 +2,42 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
+
+	"github.com/spf13/cobra"
 )
 
 func main() {
+	var rootCmd = &cobra.Command{
+		Use:   "hyperdrive",
+		Short: "Hyperdrive initialization and Rocketpool service status check",
+		Run:   runHyperdrive,
+	}
+
+	var helloCmd = &cobra.Command{
+		Use:   "hello",
+		Short: "Prints 'Hello, World!'",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Hello, World!")
+		},
+	}
+
+	rootCmd.AddCommand(helloCmd)
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+}
+
+func runHyperdrive(cmd *cobra.Command, args []string) {
 	fmt.Println("Hyperdrive initializing...")
 	rpServiceStatus := exec.Command("rocketpool", "service", "status")
 	out, err := rpServiceStatus.Output()
 	if err != nil {
-		fmt.Println("Error checking rp service status", err)
+		fmt.Println("Error checking rp service status:", err)
+		return
 	}
 	fmt.Println(string(out))
 }
