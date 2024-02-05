a3ff517529c177c109a5187e10223831bcbd1e7d
Joe Clapis <jclapis@outlook.com>
2024-02-05 15:36:34 -0500

    Validate deposit data signatures upon generation

--- daemon-utils/validator/utils/deposit-data.go
+++ daemon-utils/validator/utils/deposit-data.go
@@ -7,6 +7,9 @@ import (
 	"github.com/nodeset-org/eth-utils/beacon"
 	"github.com/nodeset-org/hyperdrive/shared"
 	"github.com/nodeset-org/hyperdrive/shared/types"
+	"github.com/prysmaticlabs/prysm/v4/beacon-chain/core/signing"
+	prdeposit "github.com/prysmaticlabs/prysm/v4/contracts/deposit"
+	ethpb "github.com/prysmaticlabs/prysm/v4/proto/prysm/v1alpha1"
 	eth2types "github.com/wealdtech/go-eth2-types/v2"
 )
 
@@ -53,6 +56,12 @@ func GetDepositData(validatorKey *eth2types.BLSPrivateKey, withdrawalCredentials
 		return types.ExtendedDepositData{}, err
 	}
 
+	// Make sure everything is correct
+	err = validateDepositInfo(genesisForkVersion, depositAmount, beacon.ValidatorPubkey(dd.PublicKey), withdrawalCredentials, beacon.ValidatorSignature(depositData.Signature))
+	if err != nil {
+		return types.ExtendedDepositData{}, fmt.Errorf("deposit data failed signature validation: %w", err)
+	}
+
 	// Create the extended data
 	return types.ExtendedDepositData{
 		PublicKey:             depositData.PublicKey,
@@ -65,4 +74,25 @@ func GetDepositData(validatorKey *eth2types.BLSPrivateKey, withdrawalCredentials
 		NetworkName:           string(network),
 		HyperdriveVersion:     shared.HyperdriveVersion,
 	}, nil
+}
+
+func validateDepositInfo(genesisForkVersion []byte, depositAmount uint64, pubkey beacon.ValidatorPubkey, withdrawalCredentials common.Hash, signature beacon.ValidatorSignature) error {
+
+	// Get the deposit domain based on the eth2 config
+	depositDomain, err := signing.ComputeDomain(eth2types.DomainDeposit, genesisForkVersion, eth2types.ZeroGenesisValidatorsRoot)
+	if err != nil {
+		return err
+	}
+
+	// Create the deposit struct
+	depositData := new(ethpb.Deposit_Data)
+	depositData.Amount = depositAmount
+	depositData.PublicKey = pubkey[:]
+	depositData.WithdrawalCredentials = withdrawalCredentials[:]
+	depositData.Signature = signature[:]
+
+	// Validate the signature
+	err = prdeposit.VerifyDepositSignature(depositData, depositDomain)
+	return err
+
 }
--- go.mod
+++ go.mod
@@ -18,6 +18,7 @@ require (
 	golang.org/x/sync v0.6.0
 	golang.org/x/term v0.16.0
 	gopkg.in/yaml.v2 v2.4.0
+	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
@@ -43,13 +44,16 @@ require (
 	github.com/gdamore/encoding v1.0.0 // indirect
 	github.com/go-ole/go-ole v1.3.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/gorilla/websocket v1.5.1 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.0.1 // indirect
 	github.com/herumi/bls-eth-go-binary v1.33.0 // indirect
 	github.com/ipfs/go-cid v0.4.1 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/minio/highwayhash v1.0.2 // indirect
 	github.com/minio/sha256-simd v1.0.1 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/mmcloughlin/addchain v0.4.0 // indirect
@@ -90,8 +94,9 @@ require (
 	golang.org/x/net v0.20.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/tools v0.17.0 // indirect
+	google.golang.org/genproto v0.0.0-20230410155749-daa745c078e1 // indirect
+	google.golang.org/grpc v1.56.3 // indirect
 	google.golang.org/protobuf v1.32.0 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
 	gotest.tools/v3 v3.5.1 // indirect
 	lukechampine.com/blake3 v1.2.1 // indirect
 	rsc.io/tmplfunc v0.0.3 // indirect
