0496d5050b90a7e35d18255975a345002914dc4f
huy <huy@nodeset.io>
2024-02-08 12:27:04 -0800

    working endpoint

--- modules/stakewise/stakewise-daemon/server/status/get-active-validators.go
+++ modules/stakewise/stakewise-daemon/server/status/get-active-validators.go
@@ -6,11 +6,9 @@ import (
 	"net/url"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
-	"github.com/ethereum/go-ethereum/common"
 	"github.com/gorilla/mux"
 	"github.com/nodeset-org/hyperdrive/daemon-utils/server"
 	"github.com/nodeset-org/hyperdrive/shared/types/api"
-	"github.com/nodeset-org/hyperdrive/shared/utils/input"
 )
 
 // ===============
@@ -26,7 +24,7 @@ func (f *statusGetActiveValidatorsContextFactory) Create(args url.Values) (*stat
 		handler: f.handler,
 	}
 	inputErrs := []error{
-		server.ValidateArg("root", args, input.ValidateHash, &c.root),
+		// server.ValidateArg("root", args, input.ValidateHash, &c.root),
 	}
 	return c, errors.Join(inputErrs...)
 }
@@ -44,24 +42,18 @@ func (f *statusGetActiveValidatorsContextFactory) RegisterRoute(router *mux.Rout
 
 type statusGetActiveValidatorsContext struct {
 	handler *StatusHandler
-	root    common.Hash
+	// root    common.Hash
 }
 
 func (c *statusGetActiveValidatorsContext) PrepareData(data *api.SuccessData, opts *bind.TransactOpts) error {
 	fmt.Printf("statusGetActiveValidatorsContext.PrepareData data: %+v\n", data)
-	// sp := c.handler.serviceProvider
-	// ec := sp.GetEthClient()
-	// res := sp.GetResources()
-	// txMgr := sp.GetTransactionManager()
-
-	// vault, err := swcommon.NewStakewiseVault(res.Vault, ec, txMgr)
-	// if err != nil {
-	// 	return fmt.Errorf("error creating Stakewise Vault binding: %w", err)
-	// }
-
-	// data.TxInfo, err = vault.SetDepositDataRoot(c.root, opts)
-	// if err != nil {
-	// 	return fmt.Errorf("error creating SetDepositDataRoot TX: %w", err)
-	// }
+	sp := c.handler.serviceProvider
+	w := sp.GetWallet()
+	privateKeys, err := w.GetAllPrivateKeys()
+	// TODO: Derive pubkeys and return
+	if err != nil {
+		return fmt.Errorf("error getting private keys: %w", err)
+	}
+	fmt.Printf("statusGetActiveValidatorsContext.PrepareData privateKeys: %+v\n", privateKeys)
 	return nil
 }
