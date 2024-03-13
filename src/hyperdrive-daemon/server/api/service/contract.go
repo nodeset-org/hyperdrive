e80c40cf7a2a3b7ab0a24dc3c2093b9722fd8f6d
huy <huy@nodeset.io>
2024-03-13 09:27:14 -0700

    WIP

---
+++ src/hyperdrive-daemon/server/api/service/contract.go
@@ -0,0 +1,39 @@
+package service
+
+import (
+	"fmt"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/nodeset-org/hyperdrive/hyperdrive-daemon/server/api/contract"
+)
+
+const (
+	UserSettingPath string = ".rocketpool/user-settings.yml"
+)
+
+// type ValidatorAccountCreator interface {
+// 	CreateNewValidatorAccount(nodeOperator common.Address) (*common.Hash, error)
+// }
+
+type RewardsClaimService struct {
+	ContractService *contract.Contract
+}
+
+func NewRewardsClaimService(contractService *contract.Contract) *RewardsClaimService {
+	return &RewardsClaimService{
+		ContractService: contractService,
+	}
+}
+
+func (vs *RewardsClaimService) ClaimRewards(address common.Address) (*common.Hash, error) {
+	tx, err := vs.ContractService.Transact(&bind.TransactOpts{}, "harvest", address)
+
+	if err != nil {
+		return nil, err
+	}
+
+	tx_hash := tx.Hash()
+	fmt.Printf("Transaction hash: %s\n", tx_hash.String())
+	return &tx_hash, nil
+}
