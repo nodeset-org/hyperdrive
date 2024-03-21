d6bb78eca4c646ed6c2b5c4864e7f7ec470fcb21
huy <huy@nodeset.io>
2024-03-21 09:00:19 -0700

    convert to camel case

--- src/hyperdrive-cli/commands/stakewise/status/get-node-status.go
+++ src/hyperdrive-cli/commands/stakewise/status/get-node-status.go
@@ -2,10 +2,27 @@ package status
 
 import (
 	"fmt"
+	"strings"
+	"unicode"
 
 	"github.com/nodeset-org/hyperdrive/hyperdrive-cli/client"
 	"github.com/urfave/cli/v2"
 )
+
+func camelToSnake(input string) string {
+	var result strings.Builder
+	for i, r := range input {
+		if unicode.IsUpper(r) {
+			if i > 0 {
+				result.WriteRune('_')
+			}
+			result.WriteRune(unicode.ToLower(r))
+		} else {
+			result.WriteRune(r)
+		}
+	}
+	return result.String()
+}
 
 func getNodeStatus(c *cli.Context) error {
 	sw := client.NewStakewiseClientFromCtx(c)
@@ -20,10 +37,9 @@ func getNodeStatus(c *cli.Context) error {
 		fmt.Printf("%v: %v\n", pubKey, status)
 	}
 
-	// TODO: Uncomment once implemented
 	fmt.Printf("\n\nNodeset Statuses:\n")
 	for pubKey, status := range response.Data.NodesetStatus {
-		fmt.Printf("%v: %v\n", pubKey, status)
+		fmt.Printf("%v: %v\n", pubKey, camelToSnake(string(status)))
 	}
 
 	return nil
