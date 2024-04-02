871a3ffcfde6ae0ae8b233797b1655a535cd928d
huy <huy@nodeset.io>
2024-04-02 11:43:05 -0700

    ref vc implmenetaions

--- src/modules/constellation/shared/config/constellation-config.go
+++ src/modules/constellation/shared/config/constellation-config.go
@@ -22,11 +22,27 @@ type ConstellationConfig struct {
 
 	// Custom command line flags
 	AdditionalOpFlags config.Parameter[string]
+
+	// Validator client configs
+	VcCommon   *config.ValidatorClientCommonConfig
+	Lighthouse *config.LighthouseVcConfig
+	Lodestar   *config.LodestarVcConfig
+	Nimbus     *config.NimbusVcConfig
+	Prysm      *config.PrysmVcConfig
+	Teku       *config.TekuVcConfig
 }
 
 // The title for the config
 func (cfg *ConstellationConfig) GetTitle() string {
 	return "Constellation"
+}
+
+// Get the parameters for this config
+func (cfg *ConstellationConfig) GetParameters() []config.IParameter {
+	return []config.IParameter{
+		&cfg.Enabled,
+		&cfg.AdditionalOpFlags,
+	}
 }
 
 // Generates a new Constellation config
@@ -61,7 +77,44 @@ func NewConstellationConfig(hdCfg *hdconfig.HyperdriveConfig) *ConstellationConf
 			},
 		},
 	}
+
+	cfg.VcCommon = config.NewValidatorClientCommonConfig()
+	cfg.Lighthouse = config.NewLighthouseVcConfig()
+	cfg.Lodestar = config.NewLodestarVcConfig()
+	cfg.Nimbus = config.NewNimbusVcConfig()
+	cfg.Prysm = config.NewPrysmVcConfig()
+	cfg.Teku = config.NewTekuVcConfig()
+	cfg.Lighthouse.ContainerTag.Default[hdconfig.Network_HoleskyDev] = cfg.Lighthouse.ContainerTag.Default[config.Network_Holesky]
+	cfg.Lodestar.ContainerTag.Default[hdconfig.Network_HoleskyDev] = cfg.Lodestar.ContainerTag.Default[config.Network_Holesky]
+	cfg.Nimbus.ContainerTag.Default[hdconfig.Network_HoleskyDev] = cfg.Nimbus.ContainerTag.Default[config.Network_Holesky]
+	cfg.Prysm.ContainerTag.Default[hdconfig.Network_HoleskyDev] = cfg.Prysm.ContainerTag.Default[config.Network_Holesky]
+	cfg.Teku.ContainerTag.Default[hdconfig.Network_HoleskyDev] = cfg.Teku.ContainerTag.Default[config.Network_Holesky]
+
 	return cfg
+}
+
+// Get the sections underneath this one
+func (cfg *ConstellationConfig) GetSubconfigs() map[string]config.IConfigSection {
+	return map[string]config.IConfigSection{
+		ids.VcCommonID:   cfg.VcCommon,
+		ids.LighthouseID: cfg.Lighthouse,
+		ids.LodestarID:   cfg.Lodestar,
+		ids.NimbusID:     cfg.Nimbus,
+		ids.PrysmID:      cfg.Prysm,
+		ids.TekuID:       cfg.Teku,
+	}
+}
+
+// ===================
+// === Module Info ===
+// ===================
+
+func (cfg *ConstellationConfig) GetApiLogFileName() string {
+	return hdconfig.ApiLogName
+}
+
+func (cfg *ConstellationConfig) GetTasksLogFileName() string {
+	return hdconfig.TasksLogName
 }
 
 // The module name
