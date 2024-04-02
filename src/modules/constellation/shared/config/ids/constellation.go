105eca1b1ddc46e41456be3b31b1a516d405abf6
huy <huy@nodeset.io>
2024-04-02 11:31:53 -0700

    wip

---
+++ src/modules/constellation/shared/config/ids/constellation.go
@@ -0,0 +1,17 @@
+package ids
+
+const (
+	// Param IDs
+	ConstellationEnableID  string = "enable"
+	OperatorContainerTagID string = "operatorContainerTag"
+	AdditionalOpFlagsID    string = "additionalOpFlags"
+	VerifyDepositRootsID   string = "verifyDepositRoots"
+
+	// Subconfig IDs
+	VcCommonID   string = "common"
+	LighthouseID string = "lighthouse"
+	LodestarID   string = "lodestar"
+	NimbusID     string = "nimbus"
+	PrysmID      string = "prysm"
+	TekuID       string = "teku"
+)
--- src/modules/constellation/shared/config/types.go
+++ src/modules/constellation/shared/config/types.go
@@ -1,9 +1,11 @@
 package constconfig
 
-import "github.com/nodeset-org/hyperdrive/shared/config"
+import (
+	"github.com/rocket-pool/node-manager-core/config"
+)
 
 const (
-	// The constellation Hyperdrive daemon
+	// The Constellation Hyperdrive daemon
 	ContainerID_ConstellationDaemon config.ContainerID = "constellation_daemon"
 	// The constellation Validator client
 	ContainerID_ConstellationValidator config.ContainerID = "constellation_vc"
