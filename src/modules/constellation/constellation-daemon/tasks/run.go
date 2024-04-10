6be84f844d2a1402d571b32b5de0572b468f3f2e
huy <huy@nodeset.io>
2024-04-10 09:21:09 -0700

    wip

--- src/modules/constellation/constellation-daemon/tasks/run.go
+++ src/modules/constellation/constellation-daemon/tasks/run.go
@@ -9,6 +9,7 @@ import (
 	"github.com/rocket-pool/node-manager-core/beacon"
 	"github.com/rocket-pool/node-manager-core/eth"
 	"github.com/rocket-pool/node-manager-core/log"
+	"github.com/rocket-pool/node-manager-core/utils"
 	"github.com/rocket-pool/rocketpool-go/rocketpool"
 	"github.com/rocket-pool/smartnode/v2/rocketpool-daemon/node"
 	"github.com/rocket-pool/smartnode/v2/rocketpool-daemon/node/collectors"
@@ -52,70 +53,72 @@ func NewTaskLoop(sp *constcommon.ConstellationServiceProvider, wg *sync.WaitGrou
 	return taskLoop
 }
 
-// // Run daemon
-// func (t *TaskLoop) Run() error {
-// 	// Initialize tasks
-// 	updateDepositData := NewUpdateDepositDataTask(t.ctx, t.sp, t.logger)
-// 	sendExitData := NewSendExitData(t.ctx, t.sp, t.logger)
+// Run daemon
+func (t *TaskLoop) Run() error {
+	// Initialize tasks
+	// 	updateDepositData := NewUpdateDepositDataTask(t.ctx, t.sp, t.logger)
+	// 	sendExitData := NewSendExitData(t.ctx, t.sp, t.logger)
 
-// 	// Run the loop
-// 	t.wg.Add(1)
-// 	go func() {
-// 		for {
-// 			err := t.sp.WaitEthClientSynced(t.ctx, false) // Force refresh the primary / fallback EC status
-// 			if err != nil {
-// 				t.logger.Error(err.Error())
-// 				if utils.SleepWithCancel(t.ctx, taskCooldown) {
-// 					break
-// 				}
-// 				continue
-// 			}
+	// Run the loop
+	t.wg.Add(1)
+	go func() {
+		for {
+			err := t.sp.ServiceProvider.WaitEthClientSynced(t.ctx, false) // Force refresh the primary / fallback EC status
+			if err != nil {
+				t.logger.Error(err.Error())
+				if utils.SleepWithCancel(t.ctx, taskCooldown) {
+					break
+				}
+				continue
+			}
 
-// 			// Check the BC status
-// 			err = t.sp.WaitBeaconClientSynced(t.ctx, false) // Force refresh the primary / fallback BC status
-// 			if err != nil {
-// 				t.logger.Error(err.Error())
-// 				if utils.SleepWithCancel(t.ctx, taskCooldown) {
-// 					break
-// 				}
-// 				continue
-// 			}
+			// Check the BC status
+			err = t.sp.ServiceProvider.WaitBeaconClientSynced(t.ctx, false) // Force refresh the primary / fallback BC status
+			if err != nil {
+				t.logger.Error(err.Error())
+				if utils.SleepWithCancel(t.ctx, taskCooldown) {
+					break
+				}
+				continue
+			}
 
-// 			// Tasks start here
+			// Tasks start here
 
-// 			// Update deposit data from the NodeSet server
-// 			if err := updateDepositData.Run(); err != nil {
-// 				t.logger.Error(err.Error())
-// 			}
-// 			if utils.SleepWithCancel(t.ctx, taskCooldown) {
-// 				break
-// 			}
+			// // Update deposit data from the NodeSet server
+			// if err := updateDepositData.Run(); err != nil {
+			// 	t.logger.Error(err.Error())
+			// }
+			// if utils.SleepWithCancel(t.ctx, taskCooldown) {
+			// 	break
+			// }
 
-// 			// Submit missing exit messages to the NodeSet server
-// 			if err := sendExitData.Run(); err != nil {
-// 				t.logger.Error(err.Error())
-// 			}
+			// // Submit missing exit messages to the NodeSet server
+			// if err := sendExitData.Run(); err != nil {
+			// 	t.logger.Error(err.Error())
+			// }
 
-// 			// Tasks end here
+			// Tasks end here
 
-// 			if utils.SleepWithCancel(t.ctx, tasksInterval) {
-// 				break
-// 			}
-// 		}
+			if utils.SleepWithCancel(t.ctx, tasksInterval) {
+				break
+			}
+		}
 
-// 		// Signal the task loop is done
-// 		t.wg.Done()
-// 	}()
+		// Signal the task loop is done
+		t.wg.Done()
+	}()
 
-// 	/*
-// 		// Run metrics loop
-// 		go func() {
-// 			err := runMetricsServer(sp, log.NewColorLogger(MetricsColor), stateLocker)
-// 			if err != nil {
-// 				errorLog.Println(err)
-// 			}
-// 			wg.Done()
-// 		}()
-// 	*/
-// 	return nil
-// }
+	/*
+
+		// Run metrics loop
+		go func() {
+			err := runMetricsServer(sp, log.NewColorLogger(MetricsColor), stateLocker)
+			if err != nil {
+				errorLog.Println(err)
+			}
+			wg.Done()
+		}()
+
+	*/
+	return nil
+}
