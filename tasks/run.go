195be498d3658416603c32ed01a6bf4bc4989539
Joe Clapis <jclapis@outlook.com>
2024-05-01 11:49:44 -0400

    Added an HD wallet waiter to module-utils

--- tasks/run.go
+++ tasks/run.go
@@ -2,6 +2,8 @@ package tasks
 
 import (
 	"context"
+	"log/slog"
+	"strings"
 	"sync"
 	"time"
 
@@ -21,60 +23,63 @@ const (
 	UpdateDepositDataColor = color.FgHiWhite
 )
 
+type waitUntilReadyResult int
+
+const (
+	waitUntilReadyExit waitUntilReadyResult = iota
+	waitUntilReadyContinue
+	waitUntilReadySuccess
+)
+
 type TaskLoop struct {
+	// Services
 	ctx    context.Context
 	logger *log.Logger
 	sp     *common.ServiceProvider
 	wg     *sync.WaitGroup
+
+	// Internal
+	wasExecutionClientSynced bool
+	wasBeaconClientSynced    bool
 }
 
 func NewTaskLoop(sp *common.ServiceProvider, wg *sync.WaitGroup) *TaskLoop {
+	logger := sp.GetTasksLogger()
+	ctx := logger.CreateContextWithLogger(sp.GetBaseContext())
 	taskLoop := &TaskLoop{
 		sp:     sp,
-		logger: sp.GetTasksLogger(),
+		logger: logger,
+		ctx:    ctx,
 		wg:     wg,
+
+		wasExecutionClientSynced: true,
+		wasBeaconClientSynced:    true,
 	}
-	taskLoop.ctx = taskLoop.logger.CreateContextWithLogger(sp.GetBaseContext())
 	return taskLoop
 }
 
 // Run daemon
 func (t *TaskLoop) Run() error {
-	// Initialize tasks
-
-	// Run the loop
+	// Run task loop
 	t.wg.Add(1)
 	go func() {
+		defer t.wg.Done()
+
 		for {
-			// Check the EC status
-			err := t.sp.WaitEthClientSynced(t.ctx, false) // Force refresh the primary / fallback EC status
-			if err != nil {
-				t.logger.Error(err.Error())
-				if utils.SleepWithCancel(t.ctx, taskCooldown) {
-					break
-				}
+			// Make sure all of the resources are ready for task processing
+			readyResult := t.waitUntilReady()
+			switch readyResult {
+			case waitUntilReadyExit:
+				return
+			case waitUntilReadyContinue:
 				continue
 			}
 
-			// Check the BC status
-			err = t.sp.WaitBeaconClientSynced(t.ctx, false) // Force refresh the primary / fallback BC status
-			if err != nil {
-				t.logger.Error(err.Error())
-				if utils.SleepWithCancel(t.ctx, taskCooldown) {
-					break
-				}
-				continue
-			}
-
-			// Tasks go here
-
-			if utils.SleepWithCancel(t.ctx, tasksInterval) {
-				break
+			// === Task execution ===
+			if t.runTasks() {
+				return
 			}
 		}
-
-		// Signal the task loop is done
-		t.wg.Done()
 	}()
 
 	/*
@@ -88,4 +93,72 @@ func (t *TaskLoop) Run() error {
 		}()
 	*/
 	return nil
+}
+
+// Wait until the chains and other resources are ready to be queried
+// Returns true if the owning loop needs to exit, false if it can continue
+func (t *TaskLoop) waitUntilReady() waitUntilReadyResult {
+	// Check the EC status
+	err := t.sp.WaitEthClientSynced(t.ctx, false) // Force refresh the primary / fallback EC status
+	if err != nil {
+		errMsg := err.Error()
+		if strings.Contains(errMsg, "context canceled") {
+			return waitUntilReadyExit
+		}
+		t.wasExecutionClientSynced = false
+		t.logger.Error("Execution Client not synced. Waiting for sync...", slog.String(log.ErrorKey, errMsg))
+		return t.sleepAndReturnReadyResult()
+	}
+
+	if !t.wasExecutionClientSynced {
+		t.logger.Info("Execution Client is now synced.")
+		t.wasExecutionClientSynced = true
+	}
+
+	// Check the BC status
+	err = t.sp.WaitBeaconClientSynced(t.ctx, false) // Force refresh the primary / fallback BC status
+	if err != nil {
+		errMsg := err.Error()
+		if strings.Contains(errMsg, "context canceled") {
+			return waitUntilReadyExit
+		}
+		// NOTE: if not synced, it returns an error - so there isn't necessarily an underlying issue
+		t.wasBeaconClientSynced = false
+		t.logger.Error("Beacon Node not synced. Waiting for sync...", slog.String(log.ErrorKey, errMsg))
+		return t.sleepAndReturnReadyResult()
+	}
+
+	if !t.wasBeaconClientSynced {
+		t.logger.Info("Beacon Node is now synced.")
+		t.wasBeaconClientSynced = true
+	}
+
+	// Wait until the wallet has been initialized
+	err = t.sp.WaitForWallet(t.ctx)
+	if err != nil {
+		errMsg := err.Error()
+		if strings.Contains(errMsg, "context canceled") {
+			return waitUntilReadyExit
+		}
+		t.logger.Error("Error waiting for wallet initialization", slog.String(log.ErrorKey, errMsg))
+		return t.sleepAndReturnReadyResult()
+	}
+
+	return waitUntilReadySuccess
+}
+
+// Sleep on the context for the task cooldown time, and return either exit or continue
+// based on whether the context was cancelled.
+func (t *TaskLoop) sleepAndReturnReadyResult() waitUntilReadyResult {
+	if utils.SleepWithCancel(t.ctx, taskCooldown) {
+		return waitUntilReadyExit
+	} else {
+		return waitUntilReadyContinue
+	}
+}
+
+// Runs an iteration of the node tasks.
+// Returns true if the task loop should exit, false if it should continue.
+func (t *TaskLoop) runTasks() bool {
+	return utils.SleepWithCancel(t.ctx, tasksInterval)
 }
