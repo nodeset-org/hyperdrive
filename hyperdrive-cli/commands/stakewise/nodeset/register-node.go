c7e7ff4d126a34a281c01ee6de955bf2440de214
Joe Clapis <jclapis@outlook.com>
2024-06-03 10:40:37 -0400

    Added interactive prompt for email address to register checks

--- hyperdrive-cli/commands/stakewise/nodeset/register-node.go
+++ hyperdrive-cli/commands/stakewise/nodeset/register-node.go
@@ -1,8 +1,20 @@
 package nodeset
 
 import (
+	"fmt"
+	"net/mail"
+
 	"github.com/nodeset-org/hyperdrive/hyperdrive-cli/client"
+	cliutils "github.com/nodeset-org/hyperdrive/hyperdrive-cli/utils"
 	"github.com/urfave/cli/v2"
+)
+
+var (
+	RegisterEmailFlag *cli.StringFlag = &cli.StringFlag{
+		Name:    "email",
+		Aliases: []string{"e"},
+		Usage:   "Email address to register with NodeSet.",
+	}
 )
 
 func registerNode(c *cli.Context) error {
@@ -16,7 +28,20 @@ func registerNode(c *cli.Context) error {
 		return err
 	}
 
-	_, err = sw.Api.Nodeset.RegisterNode(RegisterEmailFlag.Name)
+	// Get the email
+	email := RegisterEmailFlag.Name
+	if email == "" {
+		for {
+			email = cliutils.Prompt("Enter the email address you'd like to register with NodeSet:", "^.*$", "Invalid email address, try again")
+			_, err := mail.ParseAddress(email)
+			if err == nil {
+				break
+			}
+			fmt.Println("Invalid email address, try again")
+		}
+	}
+
+	_, err = sw.Api.Nodeset.RegisterNode(email)
 
 	return err
 }
