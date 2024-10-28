31f4bf9b2f42d47bbe8c60454303614fdb09d58b
Joe Clapis <jclapis@outlook.com>
2024-10-28 16:58:18 -0400

    Swapped to su only, added deb prompt support

--- hyperdrive-cli/client/package.go
+++ hyperdrive-cli/client/package.go
@@ -8,7 +8,7 @@ import (
 	"strings"
 )
 
-// Starts Hyperdrive as the provided user
+// Starts Hyperdrive as the provided user via `su`
 func StartServiceAsUser(uid uint32, hyperdriveBin string, configPath string) bool {
 	// Get the user from the uid
 	user, err := user.LookupId(fmt.Sprintf("%d", uid))
@@ -24,79 +24,19 @@ func StartServiceAsUser(uid uint32, hyperdriveBin string, configPath string) boo
 		hyperdriveCmd = append(hyperdriveCmd, "--allow-root")
 	}
 	hyperdriveCmd = append(hyperdriveCmd, "--config-path", configPath, "service", "start", "-y")
+	hyperdriveCmdStr := fmt.Sprintf("%s", strings.Join(hyperdriveCmd, " "))
 
-	// Check for sudo first
-	sudo := "sudo"
-	exists, _ := checkIfCommandExists(sudo)
-	if exists {
-		err := startServiceAsUserViaSudo(name, hyperdriveCmd)
-		if err == nil {
-			return true
-		}
-		fmt.Printf("WARN: Error starting Hyperdrive as user %d via sudo: %s\n", uid, err.Error())
+	// Run `su` to start Hyperdrive as the user
+	cmd := &command{
+		cmd: exec.Command("su", "-l", name, "-c", hyperdriveCmdStr),
+	}
+	err = runStartServiceCommand(cmd)
+	if err == nil {
+		return true
 	}
 
-	// Check for su next
-	su := "su"
-	exists, _ = checkIfCommandExists(su)
-	if exists {
-		err := startServiceAsUserViaSu(name, hyperdriveCmd)
-		if err == nil {
-			return true
-		}
-		fmt.Printf("WARN: Error starting Hyperdrive as user %d via su: %s\n", uid, err.Error())
-	}
-
-	/*
-		// Check for doas next
-		doas := "doas"
-		exists, _ = checkIfCommandExists(doas)
-		if exists {
-			err := startServiceAsUserViaDoas(name, hyperdriveCmd)
-			if err == nil {
-				return true
-			}
-			fmt.Printf("WARN: Error starting Hyperdrive as user %d via doas: %s\n", uid, err.Error())
-		}
-	*/
-
-	fmt.Printf("ERROR: All privilege escalation commands to start Hyperdrive as user %d failed or were not found.\n", uid)
+	fmt.Printf("WARN: Error starting Hyperdrive as user '%s' (%d): %s\n", name, uid, err.Error())
 	return false
-}
-
-// Starts Hyperdrive as the provided user via `sudo`
-func startServiceAsUserViaSudo(name string, hyperdriveCmd []string) error {
-	// Set up the args for sudo
-	args := []string{
-		"-i",
-		"-u",
-		name,
-	}
-	args = append(args, hyperdriveCmd...)
-
-	// Create the command and set the output to the terminal
-	cmd := &command{
-		cmd: exec.Command("sudo", args...),
-	}
-	return runStartServiceCommand(cmd)
-}
-
-// Starts Hyperdrive as the provided user via `su`
-func startServiceAsUserViaSu(name string, hyperdriveCmd []string) error {
-	// Set up the args for su
-	args := []string{
-		"-l",
-		name,
-		"-c",
-	}
-	hyperdriveCmdStr := fmt.Sprintf("'%s'", strings.Join(hyperdriveCmd, " "))
-	args = append(args, hyperdriveCmdStr)
-
-	// Create the command and set the output to the terminal
-	cmd := &command{
-		cmd: exec.Command("su", args...),
-	}
-	return runStartServiceCommand(cmd)
 }
 
 // Runs the given service start command
